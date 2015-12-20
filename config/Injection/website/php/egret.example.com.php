d8a01b720ff1fc618d445345ef11c0fe06424f99
renyunhuang <renyun.huang@gmail.com>
2015-12-20 18:29:18 +0800

    add Egret html5 games test

---
+++ config/Injection/website/php/egret.example.com.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @Author: renyun.huang@gmail.com
+ * @Date: 15-12-20
+ * @Time: 下午6:21
+ */
+use Symfony\Component\Routing\Route;
+use Symfony\Component\DependencyInjection\Reference;
+
+global $di, $request, $routes;
+
